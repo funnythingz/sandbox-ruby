b1cdfd786a9a7c842c7bb82a0f41845297352151
funnythingz <hiroki@hackleberry.me>
2014-06-11 18:05:08 +0900

    added sinatra-base

--- sinatra-demo/app.rb
+++ sinatra-demo/app.rb
@@ -1,14 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/base'
 
-get '/' do
-    @name = "sinatra"
-    @title = "sinatra sample"
-    erb :index
+class App < Sinatra::Base
+
+    get '/' do
+        @name = "sinatra"
+        @title = "sinatra sample"
+        erb :index
+    end
+
+    get '/:name' do
+        @name = "#{params[:name]}"
+        @title = "#{params[:name]} sample"
+        erb :index
+    end
+
 end
 
-get '/:name' do
-    @name = "#{params[:name]}"
-    @title = "#{params[:name]} sample"
-    erb :index
-end
+App.run! :host => 'localhost', :port => 4567
