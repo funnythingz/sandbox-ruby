0c2b1f2cce2bd11f36cb4b31fda9995646f571ad
funnythingz <funnythingz69+github@gmail.com>
2015-07-04 14:46:16 +0900

    Fixed

--- hey.rb
+++ hey.rb
@@ -1,8 +1,4 @@
-def main
-  a = gets.to_i
-  bc = gets.chomp.split(/\s+/).map(&:to_i).inject(0) { |sum, i| sum + i }
-  s = gets.chomp.to_s
-  print "#{a + bc} #{s}\n"
-end
-
-main
+a = gets.to_i
+b, c = gets.chomp.split(/\s+/).map(&:to_i)
+s = gets.chomp.to_s
+print "#{a + b + c} #{s}\n"
