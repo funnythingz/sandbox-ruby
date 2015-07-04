5fb8dc6b982bbe706926b356888b107ce02aa86d
funnythingz <funnythingz69+github@gmail.com>
2015-07-04 14:24:40 +0900

    try

---
+++ hey.rb
@@ -0,0 +1,11 @@
+require 'pp'
+
+def main
+  p 'ready?'
+  input_a = gets.to_i
+  input_bc = gets.split(' ').map{|v|v.to_i}.inject(0){|sum, i| sum + i}
+  input_s = gets.to_s
+  p "#{input_a + input_bc} #{input_s}"
+end
+
+main
