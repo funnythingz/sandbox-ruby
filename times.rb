216da5a1e4c6afd0c273641f6f29e249efdd0a2f
funnythingz <funnythingz69+github@gmail.com>
2015-07-06 23:14:04 +0900

    use puts

--- times.rb
+++ times.rb
@@ -1,5 +1,5 @@
 0..24.times do |hour|
   0..60.times do |minute|
-    p "#{sprintf("%0#{2}d", hour)}:#{sprintf("%0#{2}d", minute)}"
+    puts "#{sprintf("%0#{2}d", hour)}:#{sprintf("%0#{2}d", minute)}"
   end
 end
