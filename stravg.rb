785725f0cb776afcd86dd0049a38ed6df5759034
funnythingz <funnythingz69+github@gmail.com>
2015-07-16 21:27:28 +0900

    Fixed stravg

--- stravg.rb
+++ stravg.rb
@@ -11,15 +11,13 @@ end
 def main
   #in_s = gets.to_s
   in_s = '{That,This} is a {cat,dog,bea} the {hoge,hioge}.'
-  out_s = ''
   a = []
   unless in_s.match(/[\{\}]/)
     a = in_s.delete('.').split(' ').map{|v|v.size}
   else
     a = m(in_s)
   end
-  out_s = a.inject(0.0){|r, i| r += i} / a.size
-  puts "#{out_s}\n"
+  puts "#{a.inject(0.0){|r, i| r += i} / a.size}\n"
 end
 
 main
